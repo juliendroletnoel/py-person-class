e8b5199e0b4d427fb7ce157971adb2c0350c0cc6
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-01 17:22:16 -0500

    added specs on var name

--- app/main.py
+++ app/main.py
@@ -15,13 +15,13 @@ def create_person_list(people: list) -> list:
 
     for person in people:
         if person.get("wife") and person["wife"] in Person.people:
-            husband = Person.people[person["name"]]
-            wife = person["wife"]
-            husband.wife = Person.people[wife]
+            husband_obj = Person.people[person["name"]]
+            wife_name = person["wife"]
+            husband_obj.wife = Person.people[wife_name]
 
         elif person.get("husband") and person["husband"] in Person.people:
-            wife = Person.people[person["name"]]
-            husband = person["husband"]
-            wife.husband = Person.people[husband]
+            wife_obj = Person.people[person["name"]]
+            husband_name = person["husband"]
+            wife_obj.husband = Person.people[husband_name]
 
     return persons
